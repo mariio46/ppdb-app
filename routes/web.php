b05f8c5e9a91604a1bdbda844f98a5136264cb4e
Ryan Rafli <44920461+ryanrafliappe@users.noreply.github.com>
2023-11-17 14:39:01 +0800

    wip

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,5 @@ Route::get('/', HomeController::class)->name('home');
 Route::view('/landing-page', 'landing-page')->name('home');
 
 Route::view('/dashboard', 'dashboard.index')->name('dashboard');
+
+Route::view('/beranda', 'auth.siswa.dashboard')->name('siswa-dashboard');
