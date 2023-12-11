18845e55a948a066ba0f212be16b3e1cbec82645
Ryan Rafli <44920461+ryanrafliappe@users.noreply.github.com>
2023-12-11 22:46:43 +0800

    feat: Implement user data lock check for activation phase

--- resources/views/student/registration/index.blade.php
+++ resources/views/student/registration/index.blade.php
@@ -3,22 +3,29 @@
 @section('content')
   <div class="row">
     <div class="col-12">
-      @if (session()->get('stu_status_regis'))
+      @if (!session()->get('stu_is_locked'))
+        <div class="alert alert-danger p-1">
+          <p class="text-center"><x-bi-shield-lock /></p>
+          <p class="mb-0 text-center">Kamu belum mengunci data pribadi. Lakukan penguncian data terlebih dahulu untuk melakukan pendaftaran jalur.</p>
+        </div>
+      @else
+        @if (session()->get('stu_status_regis'))
+          <div class="card">
+            <div class="card-body p-2">
+              <div class="d-md-flex align-items-center">
+                <h4 class="my-1">Lihat Bukti Pendaftaran</h4>
+                <a class="btn btn-success ms-auto" id="registrationProofButton" href="">Lihat Detail</a>
+              </div>
+            </div>
+          </div>
+        @endif
         <div class="card">
-          <div class="card-body p-2">
-            <div class="d-md-flex align-items-center">
-              <h4 class="my-1">Lihat Bukti Pendaftaran</h4>
-              <a class="btn btn-success ms-auto" id="registrationProofButton" href="">Lihat Detail</a>
+          <div class="card-body px-5 pt-5">
+            <div class="row match-height" id="cardContainer">
             </div>
           </div>
         </div>
       @endif
-      <div class="card">
-        <div class="card-body px-5 pt-5">
-          <div class="row match-height" id="cardContainer">
-          </div>
-        </div>
-      </div>
     </div>
   </div>
 @endsection
