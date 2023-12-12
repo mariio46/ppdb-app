2807d088b850fa748879383b112433ac2b35e4fb
Ryan Rafli <44920461+ryanrafliappe@users.noreply.github.com>
2023-12-12 13:30:03 +0800

    Added middleware to check if the student is logged in or not

--- routes/web.php
+++ routes/web.php
@@ -13,67 +13,79 @@ Route::get('/', HomeController::class)->name('home');
 
 Route::view('/landing-page', 'landing-page')->name('home');
 
-// student routes
-Route::get('/masuk', [LoginController::class, 'index'])->name('student-login');
-Route::post('/do-login', [LoginController::class, 'doLogin'])->name('student-do-login');
-
-// Dashboard
-Route::controller(DashboardController::class)->group(function () {
-  Route::get('/data-diri', 'index');
-  Route::get('/data-diri/data-pribadi', 'viewEditPersonalData');
-  Route::get('/data-diri/edit-nilai/{semester}', 'viewEditStudentScore');
-
-  Route::get('/get-data-pribadi-siswa', 'getDataDashboard');
-  Route::get('/get-data-nilai-siswa', 'getDataScore');
-  Route::post('/first-time-login', 'postFirstTimeLogin');
-  Route::post('/personal-data/update-data', 'postUpdateStudentData');
-  Route::post('/personal-data/update-profile-picture', 'postUpdateStudentProfileImage');
-  Route::get('/personal-data/get-student-score/{semester}', 'getDataScoreBySemester');
-  Route::post('/personal-data/{semester}/update-score', 'postUpdateStudentScore');
-  Route::post('/lock-student-data', 'postLockStudentData');
-
-  Route::get('/provinces', 'getProvinceLists');
-  Route::get('/cities/{code}', 'getCityLists');
-  Route::get('/districts/{code}', 'getDistrictLists');
-  Route::get('/villages/{code}', 'getVillageLists');
+// middleware
+Route::group(['middleware' => 'student.guest'], function () {
+  // student routes
+  Route::get('/masuk', [LoginController::class, 'index'])->name('student-login');
+  Route::post('/do-login', [LoginController::class, 'doLogin'])->name('student-do-login');
 });
 
-// Registration as Pendaftaran
-Route::controller(RegistrationController::class)->group(function () {
-  Route::get('/pendaftaran', 'index');
-  Route::get('/pendaftaran/tahap/{code}', 'phase');
-  Route::get('/pendaftaran/jalur/{code}', 'track');
-  Route::get('/pendaftaran/bukti/{phaseCode}', 'proof');
+// middleware
+Route::group(['middleware' => 'student.auth'], function () {
+  // Dashboard
+  Route::controller(DashboardController::class)->group(function () {
+    Route::get('/data-diri', 'index');
+    Route::get('/data-diri/data-pribadi', 'viewEditPersonalData');
+    Route::get('/data-diri/edit-nilai/{semester}', 'viewEditStudentScore');
 
-  Route::get('/registration/get-schedules', 'getSchedules');
-  Route::get('/registration/get-data/{phase}', 'getDataByPhase');
-  Route::get('/registration/get-schedule-by-phase-code/{code}', 'getScheduleByPhaseCode');
-  Route::post('/registration/{trackCode}/register', 'postSchoolRegistration');
-});
+    Route::get('/get-data-pribadi-siswa', 'getDataDashboard');
+    Route::get('/get-data-nilai-siswa', 'getDataScore');
+    Route::post('/first-time-login', 'postFirstTimeLogin');
+    Route::post('/personal-data/update-data', 'postUpdateStudentData');
+    Route::post('/personal-data/update-profile-picture', 'postUpdateStudentProfileImage');
+    Route::get('/personal-data/get-student-score/{semester}', 'getDataScoreBySemester');
+    Route::post('/personal-data/{semester}/update-score', 'postUpdateStudentScore');
+    Route::post('/lock-student-data', 'postLockStudentData');
 
-// Status
-Route::controller(StatusController::class)->group(function () {
-  Route::get('/status', 'index');
+    Route::get('/provinces', 'getProvinceLists');
+    Route::get('/cities/{code}', 'getCityLists');
+    Route::get('/districts/{code}', 'getDistrictLists');
+    Route::get('/villages/{code}', 'getVillageLists');
+  });
 
-  Route::get('/status/get-status', 'getStatus');
-});
+  // Registration as Pendaftaran
+  Route::controller(RegistrationController::class)->group(function () {
+    Route::get('/pendaftaran', 'index');
+    Route::get('/pendaftaran/tahap/{code}', 'phase');
+    Route::get('/pendaftaran/jalur/{code}', 'track');
+    Route::get('/pendaftaran/bukti/{phaseCode}', 'proof');
 
-// School
-Route::controller(SchoolController::class)->group(function () {
-  Route::get('/sekolah', 'index');
+    Route::get('/registration/get-schedules', 'getSchedules');
+    Route::get('/registration/get-data/{phase}', 'getDataByPhase');
+    Route::get('/registration/get-schedule-by-phase-code/{code}', 'getScheduleByPhaseCode');
+    Route::post('/registration/{trackCode}/register', 'postSchoolRegistration');
+  });
 
-  Route::get('/schools/get-list', 'getSchools');
-  Route::get('/schools/by-city/{cityCode}/{schoolType}', 'getSchoolByCity');
-  Route::get('/schools/by-zone', 'getSchoolByZone');
-  Route::get('/schools/boarding-school', 'getBoardingSchool');
-  Route::get('/schools/department/{schoolId}', 'getDepartmentBySchool');
-});
+  // Status
+  Route::controller(StatusController::class)->group(function () {
+    Route::get('/status', 'index');
 
-// faq
-Route::controller(FaqController::class)->group(function () {
-  Route::get('/faq', 'index');
+    Route::get('/status/get-status', 'getStatus');
+  });
 
-  Route::get('/faq/get-data', 'getFaqData');
+  // School
+  Route::controller(SchoolController::class)->group(function () {
+    Route::get('/sekolah', 'index');
+
+    Route::get('/schools/get-list', 'getSchools');
+    Route::get('/schools/by-city/{cityCode}/{schoolType}', 'getSchoolByCity');
+    Route::get('/schools/by-zone', 'getSchoolByZone');
+    Route::get('/schools/boarding-school', 'getBoardingSchool');
+    Route::get('/schools/department/{schoolId}', 'getDepartmentBySchool');
+  });
+
+  // faq
+  Route::controller(FaqController::class)->group(function () {
+    Route::get('/faq', 'index');
+
+    Route::get('/faq/get-data', 'getFaqData');
+  });
+
+  // logout
+  Route::get('/keluar', function () {
+    session()->flush();
+    return redirect('/masuk')->withErrors(['errorMsg' => 'Kamu sudah logout.']);
+  });
 });
 
 
